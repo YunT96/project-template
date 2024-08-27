637541fa23eff047cfb52c27b8bd7856f34b5580
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-08-27 16:13:14 +0800

    Remove unnecessary files for template

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# project-restaurant-page
+# Template
+
+run 
+npm install css-loader html-loader html-webpack-plugin style-loader webpack webpack-cli webpack-dev-server webpack-merge --save-dev
--- webpack.prod.js
+++ webpack.prod.js
@@ -10,10 +10,10 @@ module.exports = {
     clean: true,
   },
 
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
+  // devtool: "eval-source-map",
+  // devServer: {
+  //   watchFiles: ["./src/template.html"],
+  // },
 
   plugins: [
     new HtmlWebpackPlugin({
